1dc0ccba821bfc3887434b5a9f9eb7b53ca174e3
Otabek <abdullohmirzayev7570@gmail.com>
2023-03-13 18:46:12 +0500

    sdf

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+
 function App() {
   return (
     <>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,85 @@
-import Header from '../Header/Header'
-
+import logo from "../image/logo.png"
 export default function Headers() {
   return (
     <>
       <div className="container">
         <div className="content">
           <header>
-            <div className="menu"></div>
-            <div className="logo"></div>
-            <div className="menu__1"></div>
+            <div className="header">
+              <div className="menu">
+                <ul>
+                  <li>
+                    <a href="/">
+                      <i class="fa-sharp fa-solid fa-phone"></i>
+                      7756
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/">
+                      <i class="fa-sharp fa-solid fa-location-dot"></i>
+                      Magazin
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/">
+                      <i class="fa-solid fa-truck-fast"></i>
+                      Yetkazib berish zonasi
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/">
+                      <i class="fa-regular fa-clock"></i>
+                      17:00-17:45
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className="logo">
+                <img src={logo} alt="" />
+              </div>
+              <div className="menu__1">
+                <ul>
+                  <li>
+                    <a href="/">Bo'sh-ohak</a>
+                  </li>
+                  <li>
+                    <a href="/">Vakansiya</a>
+                  </li>
+                  <li>
+                    <a href="/">Savol Javob</a>
+                  </li>
+                  <li>
+                    <a href="/">Hamkorlar</a>
+                  </li>
+                  <li>
+                    <a href="/">Bonus Dasturi</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </header>
+          <div className="menu_2">
+            <div className="search_panel">
+              <div className="katalog">
+                <a href="/">
+                  <i class="fa-solid fa-magnifying-glass-plus"></i>
+                  Katalog
+                </a>
+                <div className="search__1">
+                  <input type="text" placeholder="Nonushta Xarid qilmoq" />
+                  <i class="fa-solid fa-magnifying-glass-plus"></i>
+                </div>
+              </div>
+              <div className="search">
+                <div className="menu_item">
+                  <a href="/">Skidka</a>
+                  <a href="/">Kabinet</a>
+                  <a href="/">Sevimli</a>
+                  <a href="/">Korzinka</a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
